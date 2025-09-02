e8b758e8196d4df48e39d158a4ca44e089a39fba
Nylphis <92513556+Nylphis@users.noreply.github.com>
2025-09-02 11:31:17 +0200

    Update WordManager.cs

--- Assets/Script/Words/WordManager.cs
+++ Assets/Script/Words/WordManager.cs
@@ -82,7 +82,10 @@ namespace NecroMotMicon.Script.Words
         public void InterphaseStarted()
         {
             isInterphase = true;
-            draggedWord.GetComponent<BookWord>().CancelDragAndDrop();
+            if (draggedWord != null)
+            {
+                draggedWord.GetComponent<BookWord>().CancelDragAndDrop();
+            }
             WordDescriptionEnable();
             GetNextBadWords();
             foreach (BookWord word in wordsList)
